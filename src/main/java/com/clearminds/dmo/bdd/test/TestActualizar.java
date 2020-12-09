2fb6e90946494129b7162f909c85caecf60f6f36
Daniel Montes <daniel.montes2682@gmail.com>
2020-12-09 10:59:27 -0500

    Metodo actualizarEstudiante() implementado en ServicioEstudiante. Evidencias: BDDActualizarEstudiante.png y ActualizarEstudiante.png

---
+++ src/main/java/com/clearminds/dmo/bdd/test/TestActualizar.java
@@ -0,0 +1,17 @@
+package com.clearminds.dmo.bdd.test;
+
+import com.clearminds.dmo.dtos.Estudiante;
+import com.clearminds.dmo.excepciones.BDDException;
+import com.clearminds.dmo.servicios.ServicioEstudiante;
+
+public class TestActualizar {
+	public static void main(String[] args) {
+		ServicioEstudiante srvEstudiante = new ServicioEstudiante();
+		try {			
+			srvEstudiante.actualizarEstudiante(new Estudiante("Pedro", "Navaja", 2));			
+		} catch (BDDException e) {
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+		}
+	}
+}
--- src/main/java/com/clearminds/dmo/servicios/ServicioEstudiante.java
+++ src/main/java/com/clearminds/dmo/servicios/ServicioEstudiante.java
@@ -28,4 +28,23 @@ public class ServicioEstudiante extends ServicioBase {
 		}
 	}
 
+	public void actualizarEstudiante(Estudiante estudiante) throws BDDException {
+		abrirConexion();
+		System.out.println("Actualizando estudiante: " + estudiante);
+		Statement stmt = null;
+		try {
+			stmt = getConexion().createStatement();
+
+			String sql = "UPDATE estudiantes SET nombre='"+ estudiante.getNombre() + "', "
+					+ "apellido='" + estudiante.getApellido() + "' WHERE id = " + estudiante.getId();
+
+			System.out.println("Script: " + sql);
+
+			stmt.executeUpdate(sql);
+			cerrarConexion();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new BDDException("Error al actualizar estudiante");
+		}
+	}
 }
