8407514b90befa5d33a8e5dab31d9e9d3bf0925b
Daniel Montes <daniel.montes2682@gmail.com>
2020-12-09 13:29:31 -0500

    Modificado pom.xml para cambiar la version de la libreria de sql server a la 9.1.1. Evidencias cargadas

--- src/main/java/com/clearminds/dmo/bdd/test/TestServicio.java
+++ src/main/java/com/clearminds/dmo/bdd/test/TestServicio.java
@@ -9,7 +9,7 @@ public class TestServicio {
 	public static void main(String[] args) {
 		ServicioEstudiante srvEstudiante = new ServicioEstudiante();
 		try {			
-			srvEstudiante.insertarEstudiante(new Estudiante("Carlos", "Meza"));			
+			srvEstudiante.insertarEstudiante(new Estudiante("Peter", "Pan"));			
 		} catch (BDDException e) {
 			e.printStackTrace();
 			System.out.println(e.getMessage());
