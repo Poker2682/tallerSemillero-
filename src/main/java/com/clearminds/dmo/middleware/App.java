94c42dad7000fe9bb018a7011e9cb422fd818812
Daniel Montes <daniel.montes2682@gmail.com>
2020-12-09 09:01:26 -0500

    Version Inicial

---
+++ src/main/java/com/clearminds/dmo/middleware/App.java
@@ -0,0 +1,13 @@
+package com.clearminds.dmo.middleware;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
